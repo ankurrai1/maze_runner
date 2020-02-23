5f0756ecd32a04ac4f45365656dc83563dffa6a5
Ankur rai <rai.ankur0007@gmail.com>
2020-02-23 23:43:57 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import Maze.Game;
 import Maze.maze;
 public class Main {
+
     public static void greetUser(){
         System.out.println("Welcome to our game maze runner");
     }
 
     public static void main(String[] args) {
-
         greetUser();
         maze myMaze = new maze(20);
         Game game = new Game(myMaze);
